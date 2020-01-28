83742feb3b65205223f91cce6bc2615596cf16ca
gorzas <joseda87@gmail.com>
2020-01-28 16:23:52 +0100

    Style grid to have a main excerpt

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,13 +4,44 @@ import styled from 'styled-components';
 import Excerpt from './Excerpt';
 
 const Grid = styled.div`
+  background: white;
   display: flex;
 `;
 
-export default ({ posts }) => (
-  <Grid>
-    {posts && posts.slice(0, 7).map((post) => (
-      <Excerpt id={post.id} post={post} />
-    ))}
-  </Grid>
-);
+const Main = styled.aside`
+
+`;
+
+const Secondaries = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 150px 150px;
+  grid-gap: 10px;
+`;
+
+export default ({ posts }) => {
+  const mainPost = posts.length ?
+    posts[0] :
+    null;
+
+  return (
+    <Grid>
+      <Main>
+        <Excerpt
+          id={mainPost.id}
+          post={mainPost}
+        />
+      </Main>
+
+      <Secondaries>
+        {posts && posts.slice(1, 7).map((post, i) => (
+          <Excerpt
+            id={post.id}
+            key={i}
+            post={post}
+          />
+        ))}
+      </Secondaries>
+    </Grid>
+  );
+};
